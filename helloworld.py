9b3186f54c7f4c20588ccb3b8c6dbc0235db7366
Marc <Marc.herod978@gmail.com>
2022-06-01 12:39:57 +0200

    first commit

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrateur
+#
+# Created:     01/06/2022
+# Copyright:   (c) Administrateur 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+x = 'hello world'
